9f0fe3ea8822f51176fde8dd4d65f147bccc2bdf
nialldevlin <32718927+nialldevlin@users.noreply.github.com>
2020-11-13 09:40:41 -0500

    Commented vis.py

--- vis.py
+++ vis.py
@@ -1,3 +1,6 @@
+# Visualization script for debugging and testing
+
+
 import matplotlib.pyplot as plt
 import numpy as np
 import math
@@ -10,5 +13,7 @@ with open('y_train.npy', 'rb') as f:
 print("Dataset loaded")
 print("Dataset Length: " + str(len(x_train)))
 
+#Check if angles close to zero have a large number of samples
+#This could bias the results
 plt.hist(y_train, bins=np.arange(np.min(y_train), np.max(y_train), 0.01))
 plt.show()
